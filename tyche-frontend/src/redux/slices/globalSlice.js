68fa52c8c3c5155024220b6863bc526e58dd4a75
muffafa <mm.savar@gmail.com>
2024-09-04 02:50:32 +0300

    unnnecesary reducers deleted

--- tyche-frontend/src/redux/slices/globalSlice.js
+++ tyche-frontend/src/redux/slices/globalSlice.js
@@ -3,9 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   selectedNetwork: "ethereum",
   walletAddress: "",
-  txHistory: [],
-  portfolio: [], // Kullanıcının tokenlarını saklayacak
-  nfts: [], // Kullanıcının NFT'lerini saklayacak
 };
 
 const globalSlice = createSlice({
@@ -18,23 +15,8 @@ const globalSlice = createSlice({
     setWalletAddress(state, action) {
       state.walletAddress = action.payload;
     },
-    setTxHistory(state, action) {
-      state.txHistory = action.payload;
-    },
-    setPortfolio(state, action) {
-      state.portfolio = action.payload;
-    },
-    setNfts(state, action) {
-      state.nfts = action.payload;
-    },
   },
 });
 
-export const {
-  setNetwork,
-  setWalletAddress,
-  setTxHistory,
-  setPortfolio,
-  setNfts,
-} = globalSlice.actions;
+export const { setNetwork, setWalletAddress } = globalSlice.actions;
 export default globalSlice.reducer;

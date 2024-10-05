d48c6dcf243760b3ebe143cd320a026966f2c211
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2024-10-05 23:14:18 +0300

    Refactor search field in Header component

--- tyche-frontend/src/components/Header/Header.jsx
+++ tyche-frontend/src/components/Header/Header.jsx
@@ -56,34 +56,57 @@ function Header() {
       ) : (
         <header className="flex items-center justify-between mt-[71px] max-w-[915px] w-full mx-auto gap-[20px]">
           {/* Logo */}
-          <img src={tycheLogo} alt="Tyche Logo" className="min-w-[84px] min-h-[71px]" />
+          <button onClick={() => navigate("/")}>
+            <img
+              src={tycheLogo}
+              alt="Tyche Logo"
+              className="min-w-[84px] min-h-[71px]"
+            />
+          </button>
 
           <div className="flex items-center w-full relative">
             {/* Dropdown */}
             <div className="flex items-center h-[54px] rounded-full bg-tycheGreen p-[10px] absolute z-10">
-              <img src={greenEth} alt="Ethereum" className="border-[4px] border-tycheWhite rounded-full" />
+              <img
+                src={greenEth}
+                alt="Ethereum"
+                className="border-[4px] border-tycheWhite rounded-full"
+              />
               <select className="flex items-center bg-tycheGreen text-white text-[20px] px-4 py-1 h-[54px] rounded-r-full">
                 <option value="Ethereum">Ethereum</option>
               </select>
             </div>
             {/* Search Field */}
-            <input
-              type="text"
-              onChange={(e) => searchValue = e.target.value}
-              placeholder="Wallet address or tx hash"
-              className="bg-tycheBeige w-full text-tycheGray placeholder-gray-500 placeholder-[20px] px-4 py-1 h-[54px] rounded-full relative z-0 pl-[210px]"
-            />
-            <button
-              className="flex items-center justify-center h-full  bg-tycheGreen font-[300] text-white text-[20px] min-w-[80px] px-[24px] py-[14px] tracking-wide rounded-r-[60px] absolute right-0"
-              onClick={() => navigate("/transaction/" + searchValue)}
-            >
-              <img src={searchIcon} alt="Search" className="flex" />
-            </button>
+            <div className="flex items-center w-full">
+              <input
+                type="text"
+                onChange={(e) => (searchValue = e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    navigate("/transaction/" + searchValue);
+                  }
+                }}
+                placeholder="Wallet address or tx hash"
+                className="bg-tycheBeige w-full text-tycheGray placeholder-gray-500 placeholder-[20px] px-4 py-1 h-[54px] rounded-l-full relative z-0 pl-[210px]"
+              />
+              <button
+                className="flex items-center justify-center h-full  bg-tycheGreen font-[300] text-white text-[20px] min-w-[80px] px-[24px] py-[14px] tracking-wide rounded-r-[60px] right-0"
+                onClick={() => navigate("/transaction/" + searchValue)}
+              >
+                <img src={searchIcon} alt="Search" className="flex" />
+              </button>
+            </div>
           </div>
+
+          {/* Search Button */}
 
           {/* Settings Icon */}
           <button>
-            <img src={settingsIcon} alt="Settings" className="flex min-w-[44px] min-h-[44px]" />
+            <img
+              src={settingsIcon}
+              alt="Settings"
+              className="flex min-w-[44px] min-h-[44px]"
+            />
           </button>
 
           {/* Login Button */}

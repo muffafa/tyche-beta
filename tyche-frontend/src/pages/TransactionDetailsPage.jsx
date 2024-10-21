7669a6be6c154c21e5fbb44be4362167945e48c5
muffafa <mm.savar@gmail.com>
2024-10-21 08:59:06 +0300

    feat: Revamp Transaction Details page UI #1
    Redesigned the Transaction Details page to improve information clarity and user experience. The update includes a cleaner layout for transaction details, a prominent QR code section for easy sharing, and a consolidated view of transferred assets.

--- tyche-frontend/src/pages/TransactionDetailsPage.jsx
+++ tyche-frontend/src/pages/TransactionDetailsPage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import goBackIcon from "./../assets/images/icons/goBackIcon.svg";
 import copyIcon from "./../assets/images/icons/walletCopyIcon.svg";
 import shareIcon from "./../assets/images/icons/shareIcon.svg";
@@ -6,9 +7,11 @@ import rightArrowIcon from "./../assets/images/icons/rightArrowIcon.svg";
 import QRCode from "./../assets/images/qrcode.svg";
 import shortenAddress from "../utils/shortenAddress";
 import TokenCard from "../components/Content/TokenCard";
+import ZoomQRCode from "../components/Content/ZoomQRCode";
 
 function TransactionDetailsPage() {
   const navigate = useNavigate();
+  const [zoom, setZoom] = useState(false);
 
   // Extract the transaction hash from the URL
   const hash = window.location.pathname.split("/")[2];
@@ -25,124 +28,128 @@ function TransactionDetailsPage() {
   ];
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-[20px] w-[575px] mt-[71px]">
-          <div className="flex flex-row items-center justify-start gap-[20px] w-full">
-            <button
-              className="flex items-center bg-tychePrimary p-[13px] cursor-pointer rounded-full"
-              onClick={() => navigate(-1)}
-            >
-              <img src={goBackIcon} alt="Go Back" />
-            </button>
-            <p className="text-[32px] tracking-wide font-[350] text-[#646464]">
-              Transaction Details
-            </p>
-          </div>
-          <div className="w-[575px] h-auto bg-tycheLightGray rounded-[26px] px-[45px] py-[40px]">
-            <div className="flex flex-col w-full h-full">
-              <div className="flex flex-row items-center justify-between">
-                <div className="flex flex-row items-center gap-[7px]">
-                  <p className="text-[36px] font-[550] tracking-wide text-tycheBlue">
-                    {shortenAddress(hash)}
-                  </p>
-                  <button
-                    className="flex items-center cursor-pointer rounded-full"
-                    onClick={() => navigator.clipboard.writeText(hash)}
-                  >
-                    <img
-                      src={copyIcon}
-                      alt="Copy"
-                      className="w-[32px] h-[32px]"
-                    />
-                  </button>
-                </div>
-                <button className="flex flex-row gap-[5px] items-center justify-center bg-tychePrimary font-[300] text-white text-[20px] px-[10px] py-[9px] tracking-wide rounded-[60px] w-[167px] h-fit">
-                  <p>Share</p>
+    <div className="flex flex-col items-center justify-center w-full px-4 md:px-0 mt-4 md:mt-[80px]">
+      <div className="w-full max-w-[915px] flex flex-col gap-4 md:gap-[11px]">
+        <div className="flex flex-row items-center gap-[20px] w-full">
+          <button
+            className="flex items-center bg-tychePrimary p-[13px] cursor-pointer rounded-full"
+            onClick={() => navigate(-1)}
+          >
+            <img src={goBackIcon} alt="Go Back" />
+          </button>
+          <p className="text-[24px] text-tychePrimary tracking-wide font-[350]">
+            Transaction Details
+          </p>
+        </div>
+
+        {/* Top section */}
+        <div className="flex flex-col md:flex-row w-full gap-4 md:gap-[11px]">
+          {/* Top left */}
+          <div className="flex flex-col flex-grow bg-tycheLightGray rounded-[20px] p-[25px] gap-[24px]">
+            <div className="flex flex-row gap-[15px] items-center">
+              <p className="text-black text-[14px] font-bold">
+                Transaction Hash:
+              </p>
+              <div className="flex flex-row items-center">
+                <p className="text-[14px] text-tycheBlue font-[350]">
+                  {shortenAddress(hash)}
+                </p>
+                <button onClick={() => navigator.clipboard.writeText(hash)}>
                   <img
-                    src={shareIcon}
-                    alt="Share"
-                    className="w-[14px] h-[14px]"
+                    src={copyIcon}
+                    alt="Copy"
+                    className="w-[15px] h-[15px] ml-2"
                   />
                 </button>
               </div>
-              <div className="flex flex-row gap-[20px] mt-[40px] justify-between">
-                <div className="flex flex-col gap-[33px]">
-                  <div className="flex flex-col">
-                    <p className="text-[18px] font-[550] text-black">
-                      1 May 2023
-                    </p>
-                    <p className="text-[18px] font-[350] text-black">
-                      12:00:00
-                    </p>
-                  </div>
+            </div>
+            <div className="flex flex-row gap-[15px]">
+              <p className="text-black text-[14px] font-bold">Date:</p>
+              <p className="text-[14px] text-black font-[350]">
+                1 May 2023 12:00:00
+              </p>
+            </div>
+            <div className="flex flex-row gap-[15px]">
+              <p className="text-black text-[14px] font-bold">Status:</p>
+              <div className="flex flex-row items-center gap-[5px]">
+                <p className="text-[14px] text-black font-[350]">Completed</p>
+                <div className="w-[12px] h-[12px] rounded-full bg-tycheGreen" />
+              </div>
+            </div>
+            <div className="flex flex-row gap-[15px]">
+              <p className="text-black text-[14px] font-bold">Gas Fee:</p>
+              <p className="text-[14px] text-black font-[350]">0.001 USD</p>
+            </div>
+          </div>
 
-                  <div className="flex flex-row gap-[5px] items-center">
-                    <p className="text-[18px] font-[550] text-black">Status:</p>
-                    <p className="text-[18px] font-[350] text-black">
-                      Completed
-                    </p>
-                    <div className="flex items-center justify-center w-[12px] h-[12px] rounded-full bg-tycheGreen" />
-                  </div>
-                  <div className="flex flex-row gap-[5px] items-center">
-                    <p className="text-[18px] font-[550] text-black">
-                      Gas Fee:
-                    </p>
-                    <p className="text-[18px] font-[350] text-black">
-                      0.001 USD
-                    </p>
-                  </div>
-                </div>
+          {/* Top right (QR) */}
+          <div className="w-full md:w-[300px] bg-tycheLightGray rounded-[20px] p-[25px]">
+            <div className="flex flex-col items-center h-full gap-[3px]">
+              <p className="text-black text-[14px] font-bold mb-2">
+                Show This To Others!
+              </p>
+              <div className="flex flex-col items-center gap-[9px]">
+                <p className="text-black text-[8px] italic">
+                  {"*This transaction's link"}
+                </p>
                 <img
                   src={QRCode}
                   alt="QR Code"
-                  className="w-[167px] h-[167px]"
+                  className="w-[80px] h-[80px] cursor-pointer"
+                  onClick={() => setZoom(true)}
                 />
+                {zoom && <ZoomQRCode qr={QRCode} setZoom={setZoom} />}
               </div>
-              <div className="flex flex-row gap-[20px] mt-[40px] justify-between items-center">
-                <div className="flex flex-row gap-[22px]">
-                  <p className="text-[18px] font-[550] text-black">From:</p>
-                  <p className="text-[18px] font-[350] text-tycheBlue">
-                    0x12fjbfn...
-                  </p>
-                </div>
+              <p className="text-black text-[6px] italic">
+                Click and zoom to QR Code
+              </p>
+              <div className="h-[4px]" />
+              <button className="flex flex-row gap-[5px] items-center justify-center bg-tychePrimary font-[300] text-white text-[14px] px-[10px] py-[6px] tracking-wide rounded-[60px] w-full h-fit">
+                <p>Share</p>
                 <img
-                  src={rightArrowIcon}
-                  alt="Right Arrow"
-                  className="w-[16px] h-[16px] flex items-center"
+                  src={shareIcon}
+                  alt="Share"
+                  className="w-[14px] h-[14px]"
                 />
-                <div className="flex flex-row gap-[22px]">
-                  <p className="text-[18px] font-[550] text-black">To:</p>
-                  <p className="text-[18px] font-[350] text-tycheBlue">
-                    0x1237777...
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col mt-[37px]">
-                <div className="top-0 bg-tycheLightGray">
-                  <table className="w-full font-[550] text-[18px]">
-                    <tr className="flex flex-row w-full">
-                      <td className="flex w-full justify-start">Asset</td>
-                      <td className="flex w-full justify-start">Amount</td>
-                      <td className="flex w-full justify-end pr-[6px]">
-                        Value
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-
-                {/* Adjusted token list section to avoid scrolling */}
-                <div className="flex flex-col gap-[12px]">
-                  {tokens.map((token, index) => (
-                    <TokenCard key={index} token={token} />
-                  ))}
-                </div>
-              </div>
+              </button>
             </div>
           </div>
         </div>
+
+        {/* Middle section (from to) */}
+        <div className="flex flex-col w-full bg-tycheLightGray rounded-[20px] p-[25px]">
+          <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-col">
+              <p className="text-black text-[14px] font-bold">From:</p>
+              <p className="text-[14px] text-tycheBlue font-[350]">
+                0x12fjbfn...
+              </p>
+            </div>
+            <img
+              src={rightArrowIcon}
+              alt="Right Arrow"
+              className="w-[16px] h-[16px]"
+            />
+            <div className="flex flex-col items-end">
+              <p className="text-black text-[14px] font-bold">To:</p>
+              <p className="text-[14px] text-tycheBlue font-[350]">
+                0x1237777...
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom section (transferred assets) */}
+        <div className="flex flex-col w-full bg-tycheLightGray rounded-[20px] p-[25px]">
+          <p className="text-[18px] font-bold mb-[15px]">Transferred Assets</p>
+          <div className="flex flex-col gap-[12px]">
+            {tokens.map((token, index) => (
+              <TokenCard key={index} token={token} />
+            ))}
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 

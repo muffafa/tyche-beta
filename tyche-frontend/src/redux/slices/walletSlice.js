521f69ccf8da2319aa1788d1a6d0a26b33740cb3
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2024-10-25 10:36:00 +0300

    Refactor walletSlice.js to implement local storage saving of wallet addresses, closes #34

--- tyche-frontend/src/redux/slices/walletSlice.js
+++ tyche-frontend/src/redux/slices/walletSlice.js
@@ -1,6 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("savedWallets");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch {
+    return undefined;
+  }
+}
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("savedWallets", serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
+
+const initialState = loadState() || {
   addresses: [], // Kullanıcının cüzdan adresleri
 };
 
@@ -10,6 +30,7 @@ const walletSlice = createSlice({
   reducers: {
     addAddress(state, action) {
       state.addresses.push(action.payload);
+      saveState(state);
     },
     updateAddress(state, action) {
       const index = state.addresses.findIndex(
@@ -18,11 +39,13 @@ const walletSlice = createSlice({
       if (index !== -1) {
         state.addresses[index] = action.payload;
       }
+      saveState(state);
     },
     deleteAddress(state, action) {
       state.addresses = state.addresses.filter(
         (address) => address.id !== action.payload
       );
+      saveState(state);
     },
   },
 });

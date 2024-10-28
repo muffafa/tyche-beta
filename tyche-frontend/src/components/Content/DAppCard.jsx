57faedd9b42ee1c11d330f256160a7e06c8d3a41
muffafa <mm.savar@gmail.com>
2024-10-28 20:49:22 +0300

    Fix: Open DApp links in new tab
    DApp links now open in a new browser tab to provide a better user experience and prevent users from accidentally navigating away from the platform.

--- tyche-frontend/src/components/Content/DAppCard.jsx
+++ tyche-frontend/src/components/Content/DAppCard.jsx
@@ -7,19 +7,21 @@ function DAppCard({ dapp }) {
   return (
     <div className="flex flex-row items-center bg-white rounded-[20px] p-4 gap-[10px] relative">
       <div className="flex items-center justify-center min-w-[54px] min-h-[54px] max-w-[54px] max-h-[54px] rounded-full bg-tycheLightGray">
-      {dapp.sponsored && ( <img src={sponsoredDapp} alt="Sponsored DApp" className="absolute h-[64px] w-[64px] mt-2" /> )}
-        <img
-          src={dapp.image}
-          alt={dapp.name}
-          className="h-8 w-8"
-        />
+        {dapp.sponsored && (
+          <img
+            src={sponsoredDapp}
+            alt="Sponsored DApp"
+            className="absolute h-[64px] w-[64px] mt-2"
+          />
+        )}
+        <img src={dapp.image} alt={dapp.name} className="h-8 w-8" />
       </div>
       <div className="flex flex-row justify-between items-center w-full">
         <div className="">
           <p className="text-[12px] font-bold text-black">{dapp.name}</p>
           <p className="text-[8px]">{dapp.description}</p>
         </div>
-        <a href={dappLink}>
+        <a href={dappLink} target="_blank" rel="noopener noreferrer">
           <div className="flex flex-row items-center gap-[8px]">
             <p className="text-tycheBlue font-bold text-[12px]">Go</p>
             <img src={goLinkIcon} alt="Go Link" className="w-[12px] h-[12px]" />
